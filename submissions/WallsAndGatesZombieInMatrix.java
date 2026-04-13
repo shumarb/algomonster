d37a6d908b5d787a3087ea7598b6a2b8ca02a53a
Umar <shumarb@outlook.com>
2026-04-13 12:32:44 +0800

    Walls and Gates / Zombie in Matrix: Refactored solution

--- submissions/WallsAndGatesZombieInMatrix.java
+++ submissions/WallsAndGatesZombieInMatrix.java
@@ -21,6 +21,9 @@ class WallsAndGatesZombieInMatrix {
         m = dungeonMap.size();
         n = dungeonMap.get(0).size();
 
+        if (isTest) {
+            print("dungeonMap:", dungeonMap);
+        }
         for (int i = 0; i < m; i++) {
             List<Integer> row = new ArrayList<>();
             for (int j = 0; j < n; j++) {
@@ -34,32 +37,31 @@ class WallsAndGatesZombieInMatrix {
             result.add(row);
         }
         if (isTest) {
-            display("dungeonMap:", dungeonMap);
-            display("result", result);
+            print("result:", result);
         }
 
         return result;
     }
 
     private static int bfs(List<List<Integer>> grid, int row, int column) {
-        if (isTest) {
-            System.out.println("------------------------------------");
-            System.out.println("bfs @ [" + row + ", " + column + "]");
-        }
         Queue<int[]> queue = new LinkedList<>();
         boolean[][] isVisited = new boolean[m][n];
         int[] deltaColumns = new int[] {0, 1, 0, -1};
         int[] deltaRows = new int[] {-1, 0, 1, 0};
         int level = 0;
 
+        if (isTest) {
+            System.out.println("-------------------------------\nbfs @ [" + row + ", " + column + "]");
+        }
+
         queue.offer(new int[] {row, column});
         isVisited[row][column] = true;
-
         while (!queue.isEmpty()) {
             if (isTest) {
-                System.out.println(" * level: " + level + " | queue: ");
-                for (int[] e: queue) {
-                    System.out.print(Arrays.toString(e) + " ");
+                System.out.println("\nlevel: " + level);
+                System.out.print("queue: ");
+                for (int[] point: queue) {
+                    System.out.print("\n * " + Arrays.toString(point) + ": " + grid.get(point[0]).get(point[1]));
                 }
                 System.out.println();
             }
@@ -71,19 +73,22 @@ class WallsAndGatesZombieInMatrix {
                 int r = top[0];
 
                 if (grid.get(r).get(c) == 0) {
+                    if (isTest) {
+                        System.out.println("\n ** exit @ level " + level);
+                    }
                     return level;
                 }
 
-                for (int k = 0; k < deltaRows.length; k++) {
-                    int adjacentColumn = c + deltaColumns[k];
-                    int adjacentRow = r + deltaRows[k];
-
+                for (int j = 0; j < deltaRows.length; j++) {
+                    int adjacentColumn = c + deltaColumns[j];
+                    int adjacentRow = r + deltaRows[j];
                     if (adjacentColumn >= 0 && adjacentColumn < n
                             && adjacentRow >= 0 && adjacentRow < m
                             && grid.get(adjacentRow).get(adjacentColumn) != -1
                             && !isVisited[adjacentRow][adjacentColumn]) {
-                        isVisited[adjacentRow][adjacentColumn] = true;
+
                         queue.offer(new int[] {adjacentRow, adjacentColumn});
+                        isVisited[adjacentRow][adjacentColumn] = true;
                     }
                 }
             }
@@ -94,12 +99,11 @@ class WallsAndGatesZombieInMatrix {
         return key;
     }
 
-    private static void display(String s, List<List<Integer>> grid) {
+    private static void print(String s, List<List<Integer>> grid) {
         System.out.println(s);
-        for (List<Integer> row: grid) {
-            System.out.println(row);
+        for (List<Integer> e: grid) {
+            System.out.println(e);
         }
-        System.out.println("--------------------------------------");
     }
 
     public static List<String> splitWords(String s) {
